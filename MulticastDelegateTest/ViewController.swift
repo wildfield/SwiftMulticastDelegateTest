c545391e2a9c3240d7b4536a9ad23cb4946ce9f3
Dmitry Kasianchuk <wildfield@live.ru>
2015-06-28 21:59:52 +0300

    Initial Commit

---
+++ MulticastDelegateTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MulticastDelegateTest
+//
+//  Created by Dmitriy on 6/28/15.
+//  Copyright (c) 2015 Dmitry K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
